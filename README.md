f69ff20f4965b0f38ac4923458b30bffe4af8c03
Nicolas Kray <33665069+Kranysys@users.noreply.github.com>
2018-10-01 19:43:40 +0200

    ROOT PROJECT

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # TimeOff
 
 # Objectif :
@@ -33,3 +34,12 @@ processus de demande et validation de congés.
 - la réalisation d'une API web
 
 - (optionnellement) la réalisation d'un front-end web
+=======
+This is a starter project to use in an F# coding assignment working with event-sourcing and modeling a workflow.
+
+It relies on .NET core and references Expecto as a testing library/runner.
+
+Just clone the repo or download it, and get started coding and testing in F#!
+
+To get started, just dotnet run in the folder to run the Expecto tests.
+>>>>>>> 404cc1b... ALL
---
+++ TimeOffEvents/Model.fs
@@ -0,0 +1,121 @@
+﻿namespace TimeOff
+
+open System
+open EventStorage
+
+// First, we define our domain
+type User =
+    | Employee of int
+    | Manager
+
+type HalfDay = | AM | PM
+
+type Boundary = {
+    Date: DateTime
+    HalfDay: HalfDay
+}
+
+type UserId = int
+
+type TimeOffRequest = {
+    UserId: UserId
+    RequestId: Guid
+    Start: Boundary
+    End: Boundary
+}
+
+// Then our commands
+type Command =
+    | RequestTimeOff of TimeOffRequest
+    | ValidateRequest of UserId * Guid with
+    member this.UserId =
+        match this with
+        | RequestTimeOff request -> request.UserId
+        | ValidateRequest (userId, _) -> userId
+
+// And our events
+type RequestEvent =
+    | RequestCreated of TimeOffRequest
+    | RequestValidated of TimeOffRequest with
+    member this.Request =
+        match this with
+        | RequestCreated request -> request
+        | RequestValidated request -> request
+
+// We then define the state of the system,
+// and our 2 main functions `decide` and `evolve`
+module Logic =
+
+    type RequestState =
+        | NotCreated
+        | PendingValidation of TimeOffRequest
+        | Validated of TimeOffRequest with
+        member this.Request =
+            match this with
+            | NotCreated -> invalidOp "Not created"
+            | PendingValidation request
+            | Validated request -> request
+        member this.IsActive =
+            match this with
+            | NotCreated -> false
+            | PendingValidation _
+            | Validated _ -> true
+
+    let evolve state event =
+        match event with
+        | RequestCreated request -> PendingValidation request
+        | RequestValidated request -> Validated request
+
+    let getRequestState requestEvents =
+        requestEvents |> Seq.fold evolve NotCreated
+
+    // This function builds a map (i.e. a dictionary)
+    // of request states by request id, from all events
+    // associated to a user's requests
+    let getAllRequests userRequestsEvents =
+        let folder requests (event: RequestEvent) =
+            let state = defaultArg (Map.tryFind event.Request.RequestId requests) NotCreated
+            let newState = evolve state event
+            requests.Add (event.Request.RequestId, newState)
+
+        userRequestsEvents |> Seq.fold folder Map.empty
+
+    let overlapWithAnyRequest (otherRequests: TimeOffRequest seq) request =
+        false //TODO
+
+    let createRequest activeUserRequests  request =
+        if overlapWithAnyRequest activeUserRequests  request then
+            Error "Overlapping request"
+        // This DateTime.Today must go away!
+        elif request.Start.Date <= DateTime.Today then
+            Error "The request starts in the past"
+        else
+            Ok [RequestCreated request]
+
+    let validateRequest requestState =
+        match requestState with
+        | PendingValidation request ->
+            Ok [RequestValidated request]
+        | _ ->
+            Error "Request cannot be validated"
+
+    let decide (store: IStore<UserId, RequestEvent>) (command: Command) =
+        let userId = command.UserId
+        let stream = store.GetStream userId
+        let events = stream.ReadAll()
+        let userRequests = getAllRequests events
+
+        match command with
+        | RequestTimeOff request ->
+            let activeUserRequests  =
+                userRequests
+                |> Map.toSeq
+                |> Seq.map (fun (_, state) -> state)
+                |> Seq.where (fun state -> state.IsActive)
+                |> Seq.map (fun state -> state.Request)
+
+            createRequest activeUserRequests  request
+
+        | ValidateRequest (_, requestId) ->
+            let requestState = defaultArg (userRequests.TryFind requestId) NotCreated
+            validateRequest requestState
